75b6002449495048974d35eeedc1765937bf74b0
Grilled <73136925+GrilledTG@users.noreply.github.com>
2026-03-31 23:10:54 -0500

    df

--- js/score.js
+++ js/score.js
@@ -11,11 +11,11 @@ const scale = 2;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 171) {
+    if (rank > 43) {
         return 0;
     }
 
-    if (rank > 171 && percent < 100) {
+    if (rank > 43 && percent < 100) {
         return 0;
     }
 
@@ -25,7 +25,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-32.8*Math.pow(rank-1, 0.4) + rank*19) *
+    let score = (-32.8*Math.pow(rank-1, 0.4) + -rank + 852 ) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
